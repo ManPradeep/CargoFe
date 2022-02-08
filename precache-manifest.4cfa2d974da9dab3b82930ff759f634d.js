d42f9477a01655b5f8e4e3be3e23e471e71ee545
Pradeep Maurya <manpradeep25@gmail.com>
2022-02-08 22:29:32 +0530

    Updates

---
+++ precache-manifest.4cfa2d974da9dab3b82930ff759f634d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2aa85ed6ace325dbb1a4acdfd93ab8b2",
+    "url": "/CargoFe/index.html"
+  },
+  {
+    "revision": "336a8330a6b352661d2c",
+    "url": "/CargoFe/static/css/2.86aa6515.chunk.css"
+  },
+  {
+    "revision": "7e970c453e784d81c9ea",
+    "url": "/CargoFe/static/css/main.ab03d080.chunk.css"
+  },
+  {
+    "revision": "336a8330a6b352661d2c",
+    "url": "/CargoFe/static/js/2.760751c7.chunk.js"
+  },
+  {
+    "revision": "7e970c453e784d81c9ea",
+    "url": "/CargoFe/static/js/main.afe76c9e.chunk.js"
+  },
+  {
+    "revision": "b0efc42d73ba9229f536",
+    "url": "/CargoFe/static/js/runtime~main.93ab11ae.js"
+  },
+  {
+    "revision": "b735f358e58703af482142d094de78d1",
+    "url": "/CargoFe/static/media/crgLogo.b735f358.jpg"
+  },
+  {
+    "revision": "6b5a922f6aabd8ea1cf10bb140170d71",
+    "url": "/CargoFe/static/media/ship.6b5a922f.jpg"
+  }
+]);
